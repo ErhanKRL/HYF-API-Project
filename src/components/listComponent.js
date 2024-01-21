f9de23655defe11fd3b5b5cef7758967cff0a0d4
Erhan Koral <erhan.16k@gmail.com>
2024-01-21 15:57:23 +0100

    list component refactored

--- src/components/listComponent.js
+++ src/components/listComponent.js
@@ -1,6 +1,6 @@
 import { getCurrencyList } from "../utils/getCurrencyList";
 import { checkDataTimeStamp } from "../utils/checkDataTimeStamp";
-import { formatData } from "../utils/formatData";
+import { createTableComponent } from "./tableComponent";
 import { createPaginationComponent } from "./paginationComponent";
 import { data } from "../data";
 
@@ -29,7 +29,7 @@ export const createCurrencyListComponent = async (start, onClick) => {
   const currencyListingData = data.currencyListData[start].data.map(coin => {
     return diluteListingData(coin)
   })
-  const table = createTable(currencyListingData)
+  const table = createTableComponent(currencyListingData)
   const containerElement = document.createElement('div');
   containerElement.className = 'list-container';
   const totalPages = data.currencyListData[start].status.total_count;
@@ -38,74 +38,6 @@ export const createCurrencyListComponent = async (start, onClick) => {
   containerElement.appendChild(table);
   return containerElement;
 };
-
-const createTableHead = (table, data) => {
-  let thead = table.createTHead();
-  let row = thead.insertRow();
-  for (let key in data) {
-    let th = document.createElement("th");
-    let text = formatData(key, 'Title');
-    let titleText = document.createTextNode(text);
-    th.appendChild(titleText);
-    row.appendChild(th);
-  }
-}
-
-const generateTable = (table, data) => {
-  const values = ['cmcRank', 'm_cap', 'name', 'price', 'symbol', 'volume_7d', 'volume_24h'];
-  const changes = ['change_7d', 'change_24h', 'v_change_24h']
-  const tblBody = document.createElement("tbody");
-  data.forEach(dataItem =>{
-    const row = document.createElement("tr");
-    Object.keys(dataItem).forEach(key =>{
-      if(values.indexOf(key) >= 0){
-        const name = document.createElement("td");
-        const nameDiv = document.createElement("div");
-        const nameText = document.createElement("p");
-        const text = formatData(dataItem[key], key);
-        nameText.innerText = (text);
-        nameDiv.appendChild(nameText);
-        name.appendChild(nameDiv);
-        row.appendChild(name);
-      }
-      if(changes.indexOf(key) >= 0){
-        const name = document.createElement("td");
-        const nameDiv = document.createElement("div");
-        const nameSpan = document.createElement("span");
-        const icon = document.createElement("i");
-        const textData = formatData(dataItem[key], key);
-        dataItem[key] >= 0 ? nameSpan.classList.add("up"): nameSpan.classList.add("down");
-        dataItem[key] >= 0 ? icon.className = "fa fa-caret-up up" : icon.className = "fa fa-caret-down down";
-        nameDiv.appendChild(icon);
-        nameSpan.innerText = (textData)
-        nameDiv.appendChild(nameSpan);
-        name.appendChild(nameDiv);
-        row.appendChild(name);
-      }
-    })
-    
-    tblBody.appendChild(row)
-  })
-  table.appendChild(tblBody)
-}
-  
-
-const createTable = (data) => {
-  const table = document.createElement("table");
-  generateTable(table, data);
-  createTableHead(table, data[0]);
-  return table;
-};
-
-async function fetchCurrencyListData(start) {
-  try {
-    data.currencyListData[start] = await getCurrencyList(start);
-    localStorage.setItem('data', JSON.stringify(data));
-    return data.currencyListData
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  }
-}
 
 function diluteListingData(data) {
   const {
