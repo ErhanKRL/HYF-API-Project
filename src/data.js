15053e801caa5553db086f5766a7df004fa40950
erhan <erhan.16k@gmail.com>
2024-06-15 17:11:33 +0200

    netlify

--- src/data.js
+++ src/data.js
@@ -1,3 +1,5 @@
-export const API_KEY = import.meta.env.VITE_API_KEY;
-export const BASE_URL = import.meta.env.VITE_BASE_URL;
-export const data = JSON.parse(localStorage.getItem('data')) || {};
+import "dotenv/config";
+
+export const API_KEY = process.env.VITE_API_KEY;
+export const BASE_URL = process.env.VITE_BASE_URL;
+export const data = JSON.parse(localStorage.getItem("data")) || {};
